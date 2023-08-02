6c7ee39c3de2bc02e118c52d99a4250c245c5b51
FLEYreal <borisov.nikita.off@gmail.com>
2023-08-02 14:59:49 +0800

    Fixed logic mistake

--- index.js
+++ index.js
@@ -33,22 +33,25 @@ app.post('/calculate-cost', (req, res) => {
     // 1. Convert initialCost to RUB
     let rubInitialCost = initialCost * liraExchangeRate;
 
-    // 2. Calculate commission
+    // 2. Add extra charge
+    rubInitialCost += extraCharge;
+
+    // 3. Calculate commission
     for (let i = 0; i < commission.length; i++) {
 
         // If currency is TRY
         if (commission[i].currency === 'TRY') {
 
-            // If commission type is percent so add % and convert to RUB
-            if (commission[i].type === 'percent') rubInitialCost += rubInitialCost * commission[i].value / 100;
+            // If commission type is percent, just add %
+            if (commission[i].type === 'percent') rubInitialCost += rubInitialCost * (commission[i].value / 100);
 
             // If commission type is fixed, just add it to the initial cost
-            else rubInitialCost += commission[i].value;
+            else rubInitialCost += commission[i].value * liraExchangeRate;
 
         } else {
 
             // If commission type is percent, just add %
-            if (commission[i].type === 'percent') rubInitialCost += commission[i].value / liraExchangeRate;
+            if (commission[i].type === 'percent') rubInitialCost += rubInitialCost * (commission[i].value / 100);
 
             // If commission type is fixed, just add it to the initial cost
             else rubInitialCost -= commission[i].value;
@@ -56,14 +59,11 @@ app.post('/calculate-cost', (req, res) => {
         }
     }
 
-    // 3. Add extra charge
-    let result = rubInitialCost + extraCharge;
-
     // 4. Return result & log it
-    console.log("\"/calculate-cost\":", result)
+    console.log("\"/calculate-cost\":", rubInitialCost)
 
     res.json({
-        result: result
+        result: rubInitialCost
     })
 });
 
@@ -88,16 +88,16 @@ app.post('/calculate-profit', (req, res) => {
 
         if (commission[i].currency === 'TRY') {
 
-            // If commission type is percent so substract % and convert to RUB
-            if (commission[i].type === 'percent') cost -= cost * commission[i].value / 100;
+            // If commission type is percent, just substract %
+            if (commission[i].type === 'percent') cost -= cost * (commission[i].value / 100);
 
             // If commission type is fixed, just substract it from the initial cost and convert it to RUB
-            else cost -= commission[i].value;
+            else cost -= commission[i].value  * liraExchangeRate;
 
         } else {
 
             // If commission type is percent, just substract %
-            if (commission[i].type === 'percent') cost -= commission[i].value / liraExchangeRate;
+            if (commission[i].type === 'percent') cost -= cost * (commission[i].value / 100);
 
             // If commission type is fixed, just substract it from the initial cost
             else cost -= commission[i].value;
